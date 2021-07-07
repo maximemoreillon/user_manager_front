ec3997c0d018ae3233202560ef028bde368c6bb4
Maxime Moreillon <m.moreillon@gmail.com>
2021-07-07 23:15:46 +0000

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="about">
     <h1>User Manager GUI</h1>
-    <p>Version {{version}}</p>
-    <p>Developped by <a href="https://maximemoreillon.com">Maxime MOREILLON</a> </p>
-    <p>User manager API URL: {{auser_manager_api_url}}</p>
+    <p>A user management service developped by <a href="https://maximemoreillon.com">Maxime MOREILLON</a> </p>
+    <h2>Services</h2>
+    <table>
+      <tr>
+        <th>Service</th>
+        <th>Version</th>
+        <th>URL</th>
+      </tr>
+      <tr
+        v-for="(service, index) in services"
+        :key="`service_${index}`">
+        <td>{{service.name}}</td>
+        <td>{{service.version}}</td>
+        <td>{{service.url}}</td>
+      </tr>
+    </table>
 
   </div>
 </template>
@@ -16,11 +29,38 @@ export default {
   components: {
 
   },
+  mounted(){
+    this.get_services_version()
+  },
+  get_services_version () {
+    this.services.forEach((service) => {
+      if (service.version) return
+      service.version = 'Connecting...'
+      this.axios.get(service.url)
+        .then(({ data }) => { service.version = data.version })
+        .catch(() => { service.version = 'Unable to connect' })
+    })
+  }
   data(){
     return {
-      version: pjson.version,
-      auser_manager_api_url: process.env.VUE_APP_USER_MANAGER_API_URL,
-      authentication_api_url: process.env.VUE_APP_AUTHENTICATION_API_URL,
+      services: [
+        {
+          name: 'User manager GUI',
+          url: window.location.origin,
+          version: pjson.version
+        },
+        {
+          name: 'User manager API',
+          url: process.env.VUE_APP_USER_MANAGER_API_URL,
+          version: null
+        },
+        {
+          name: 'Authentication API',
+          url: process.env.VUE_APP_AUTHENTICATION_API_URL,
+          version: null
+        },
+
+      ],
     }
   },
 

2380b71465805c5764e3fb1c1664ad92764dfd2c
Maxime Moreillon <m.moreillon@gmail.com>
2021-09-12 02:36:00 +0000

    updated template

--- src/main.js
+++ src/main.js
@@ -9,29 +9,6 @@ Vue.use(VueCookies)
 Vue.use(VueAxios, axios)
 
 
-/*
-router.beforeEach((to, from, next) => {
-
-  console.log('beforeEach')
-
-  // get current_user
-  if(VueCookies.get("jwt")) {
-    axios.defaults.headers.common['Authorization'] = `Bearer ${VueCookies.get('jwt')}`
-    axios.get(`${process.env.VUE_APP_USER_MANAGER_API_URL}/users/self`)
-    .then(({data}) => { store.commit('set_current_user', data) })
-    .catch(error => { console.error(error) })
-
-    next()
-  }
-  else {
-    delete axios.defaults.headers.common['Authorization']
-    //window.location.href = process.env.VUE_APP_AUTHENTICATION_FRONT_URL;
-  }
-
-  //access_control(next)
-});
-*/
-
 Vue.config.productionTip = false
 
 new Vue({

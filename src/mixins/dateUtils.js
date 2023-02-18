11d500e9d2222f3607f60c671a3a586130d395ff
Maxime Moreillon <m.moreillon@gmail.com>
2023-02-19 08:59:23 +0900

    fixed date formatting

--- src/mixins/dateUtils.js
+++ src/mixins/dateUtils.js
@@ -1,13 +1,12 @@
 export default {
-    methods: {
-        format_date(d) {
-            if (!d) return 'No date'
+  methods: {
+    format_date(d) {
+      if (!d) return "No date"
 
-            // Check if Neo4J format
-            if (d.year) return `${d.year}/${d.month}/${d.day}`
+      // Check if Neo4J format
+      if (d.year) return `${d.year}/${d.month}/${d.day}`
 
-            new Date(d).toString()
-
-        }
-    }
+      return new Date(d).toString()
+    },
+  },
 }
